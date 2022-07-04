207e02b8159c95fdae4aaeab0a98f206e4810a44
julio <julio@DESKTOP-QTDGH0L>
2022-07-04 17:24:12 -0300

    subindo projeto

---
+++ src/test/java/adid6/api/Filmes/FilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package adid6.api.Filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
